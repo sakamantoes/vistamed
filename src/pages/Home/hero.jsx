8683affa104c41fedb6b4c31b2721a821db1c815
uchenna <chinwekeleuchenna@gmail.com>
2025-05-01 10:38:26 +0100

    new change

--- src/pages/Home/hero.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Hero() {
-  return <div className=""></div>;
-}
-
-export default Hero;
--- src/pages/Home/more.jsx
+++ src/pages/Home/more.jsx
@@ -2,26 +2,32 @@ import React from "react";
 
 const moreData = [
   {
+    src: "/img/student-card.png",
     title: "Universal  Patient ID",
     body: "Secured  And UNIQUE IDENTIFIERS FOR EVERY PATIENT ",
   },
   {
+    src: "/img/ai.png",
     title: "aI Health Predictors",
     body: " Forecast health risks using machine learning",
   },
   {
+    src: "/img/timer.png",
     title: "Real-Time Access ",
     body: "Access and sync records across all facilities  ",
   },
   {
+    src: "/img/hospital.png",
     title: "Hospital integration",
     body: "connect easily via API  TO EXISTING EMRS ",
   },
   {
+    src: "/img/protection.png",
     title: "data security",
     body: "End-to-end encryption and access control  ",
   },
   {
+    src: "/img/dashboard.png",
     title: "patient dashboard",
     body: "Track Medical history, Prescription, and more.... ",
   },
@@ -34,15 +40,23 @@ function More() {
         Key Features
       </h1>
       <div className="grid md:grid-cols-3 gap-8 items-center justify-center">
-        {moreData.map(({ title, body }, i) => (
+        {moreData.map(({ title, body, src }, i) => (
           <div
             key={i}
-            className=" bg-secondary-gray w-[280px] md:w-[270px] h-[170px] rounded-md flex items-center justify-start flex-col py-3 px-2 hover:translate-y-[-10px] transition-all duration-500 autoDisplay"
+            className=" bg-secondary-gray w-[280px] md:w-[300px] h-[170px] rounded-md flex items-center justify-start flex-row py-3 px-2 hover:translate-y-[-10px] transition-all duration-500 autoDisplay"
           >
-            <h1 className="capitalized text-primary-blue font-bold text-[20px]">
-              {title}
-            </h1>
-            <p className="word-spacing-custom">{body}</p>
+            <div className="w-1/2">
+              <h1 className="capitalized text-primary-blue font-bold text-[20px]">
+                {title}
+              </h1>
+              <p className="word-spacing-custom text-[13px] text-gray-600">
+                {body}
+              </p>
+            </div>
+
+            <div className="w-1/2">
+              <img src={src} alt="icon" className="w-[180px]" />
+            </div>
           </div>
         ))}
       </div>
